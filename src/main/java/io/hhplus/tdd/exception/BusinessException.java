6b4051efcc217d5467ee8c5c546f8a55f7648f59
bje <baejungeun11@gmail.com>
2024-12-20 01:58:41 +0900

    커스텀 Exception 생성

---
+++ src/main/java/io/hhplus/tdd/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package io.hhplus.tdd.exception;
+
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode.getCode();
+    }
+
+    public String getErrorMessage() {
+        return errorCode.getMessage();
+    }
+}
